5511400ffcf98919f0c8526f02f265e978417e7b
UsmanCS365 <CS365X@gmail.com>
2025-02-13 22:10:52 +0500

    INITIAL commit

---
+++ code1.cpp
@@ -0,0 +1,360 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+struct student{
+    char name[20], roll_no[20];
+    int marks;
+    
+};
+
+struct student  st;
+void Cpp();
+void Cpp(){
+        st.marks =0;
+        char choice;
+        cout<<"C++ Quiz"<<endl;
+     cout<<"1. What is the correct syntax to output \"Hello, World!\" in C++?"<<endl;
+    cout<<"A. system.out.println(\"Hello, World!\");"<<endl;
+    cout<<"B. Console.WriteLine(\"Hello, World!\";)"<<endl;
+    cout<<"C. print(\"Hello, World!\")"<<endl;
+    cout<<"D. cout<<\"Hello, World!\";"<<endl;
+    choice =getch();
+    if(choice =='D' || choice =='d'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is D"<<endl;
+    }
+    
+    cout<<"2. Which of the following header files is used to include the basic input/output functions in C++ program?"<<endl;
+    cout<<"A. #include<stdlib.h>"<<endl;
+    cout<<"B. #include<stdexcept"<<endl;
+    cout<<"C. #include<iostream>"<<endl;
+    cout<<"D. #include<fstream>"<<endl;
+    choice =getch();
+    if(choice =='C' || choice =='c'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is C"<<endl;
+    }
+    
+    cout<<"3. Which of the following functions is used to take input from the user in C++?"<<endl;
+    cout<<"A. input()"<<endl;
+    cout<<"B. cin()"<<endl;
+    cout<<"C. scanf()"<<endl;
+    cout<<"D. Console.ReadLine()"<<endl;
+    choice =getch();
+    if(choice =='B' || choice =='b'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is B"<<endl;
+    }
+    
+
+    cout<<"4. Which statement is correct?"<<endl;
+    cout<<"A. Int a =22"<<endl;
+    cout<<"B. int a =\'2\';"<<endl;
+    cout<<"C. int a =22;"<<endl;
+    cout<<"D. None of the above"<<endl;
+    choice =getch();
+    if(choice =='C' || choice =='c'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is C"<<endl;
+
+    }
+
+
+    cout<<"5. In C++, break keyword is used for?"<<endl;
+    cout<<"A. Exitting a program"<<endl;
+    cout<<"B.  Inserting  a line break"<<endl;
+    cout<<"C. Exitting a function"<<endl;
+    cout<<"D. Terminating a loop or switch"<<endl;
+    choice =getch();
+    if(choice =='D' || choice =='d'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is D"<<endl;
+    }
+
+    
+
+
+}
+void Python();
+void Python(){
+    st.marks =0;
+        char choice;
+        cout<<"Python Quiz"<<endl;
+     cout<<"1. What is the correct syntax to output \"Hello, World!\" in Python?"<<endl;
+    cout<<"A. system.out.println(\"Hello, World!\");"<<endl;
+    cout<<"B. console.WriteLine(\"Hello, World!\";)"<<endl;
+    cout<<"C. print(\"Hello, World!\")"<<endl;
+    cout<<"D. cout<<\"Hello, World!\";"<<endl;
+    choice =getch();
+    if(choice =='C' || choice =='c'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is C"<<endl;
+    }
+    
+    cout<<"2. Which of the following modules is used for  HTTp requests in Python?"<<endl;
+    cout<<"A. Web browser"<<endl;
+    cout<<"B. Requests"<<endl;
+    cout<<"C. Sub process"<<endl;
+    cout<<"D. Youtube"<<endl;
+    choice =getch();
+    if(choice =='B' || choice =='b'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is B"<<endl;
+    }
+    
+    cout<<"3. Which of the following functions is used to take input from the user in Python?"<<endl;
+    cout<<"A. input()"<<endl;
+    cout<<"B. cin()"<<endl;
+    cout<<"C. scanf()"<<endl;
+    cout<<"D. Console.ReadLine()"<<endl;
+    choice =getch();
+    if(choice =='A' || choice =='a'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is A"<<endl;
+    }
+    
+
+    cout<<"4. Which statement is correct?"<<endl;
+    cout<<"A. Import pi from math"<<endl;
+    cout<<"B. import pi from math"<<endl;
+    cout<<"C. import math.pi"<<endl;
+    cout<<"D. from math import pi"<<endl;
+    choice =getch();
+    if(choice =='D' || choice =='d'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is D"<<endl;
+
+    }
+
+
+    cout<<"5. In Python, def keyword is used for?"<<endl;
+    cout<<"A. Exitting a program"<<endl;
+    cout<<"B.  Inserting  a line break"<<endl;
+    cout<<"C. Defining  a function"<<endl;
+    cout<<"D. Terminating a loop or switch"<<endl;
+    choice =getch();
+    if(choice =='C' || choice =='c'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is C"<<endl;
+    }
+
+    
+
+}
+
+void Csharp();
+void Csharp(){
+    st.marks =0;
+        char choice;
+        cout<<"C# Quiz"<<endl;
+     cout<<"1. What is the correct syntax to output \"Hello, World!\" in C#?"<<endl;
+    cout<<"A. system.out.println(\"Hello, World!\");"<<endl;
+    cout<<"B. console.WriteLine(\"Hello, World!\";)"<<endl;
+    cout<<"C. print(\"Hello, World!\")"<<endl;
+    cout<<"D. cout<<\"Hello, World!\";"<<endl;
+    choice =getch();
+    if(choice =='B' || choice =='b'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is B"<<endl;
+    }
+    
+    cout<<"2. In C#, a block of code used to perform a specific task is called?"<<endl;
+    cout<<"A. Main()"<<endl;
+    cout<<"B. Method"<<endl;
+    cout<<"C. Function"<<endl;
+    cout<<"D. Class"<<endl;
+    choice =getch();
+    if(choice =='B' || choice =='b'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is B"<<endl;
+    }
+    
+    cout<<"3. Which of the following functions is used to take input from the user in C#?"<<endl;
+    cout<<"A. input()"<<endl;
+    cout<<"B. cin()"<<endl;
+    cout<<"C. scanf()"<<endl;
+    cout<<"D. Console.ReadLine()"<<endl;
+    choice =getch();
+    if(choice =='D' || choice =='d'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is D"<<endl;
+    }
+    
+
+    cout<<"4. Which statement is correct?"<<endl;
+    cout<<"A. string  a =22"<<endl;
+    cout<<"B. int a =\'2\';"<<endl;
+    cout<<"C. char  a =22;"<<endl;
+    cout<<"D. None of the above"<<endl;
+    choice =getch();
+    if(choice =='D' || choice =='d'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is D"<<endl;
+
+    }
+
+
+    cout<<"5. In C#, createkeyword is used for?"<<endl;
+    cout<<"A. exitting a program"<<endl;
+    cout<<"B.  inserting  a line break"<<endl;
+    cout<<"C. Keyword doesn't exist"<<endl;
+    cout<<"D. terminating a loop or switch"<<endl;
+    choice =getch();
+    if(choice =='C' || choice =='c'){
+        cout<<"Your answer is correct!!!"<<endl;
+        st.marks++;
+
+    }
+    else{
+        cout<<"Wrong answer"<<endl;
+        cout<<"Correct answer is C"<<endl;
+    }
+
+    
+
+}
+void result();
+void result(){
+    int per =0;
+    
+    cout<<"Student's Name: "<<st.name<<endl;
+    cout<<"Student's Roll No. "<<st.roll_no<<endl;
+    cout<<"Marks: "<<st.marks<<" out of 5"<<endl;
+    per =100 *st.marks/5;
+    cout<<"Percentage: "<<per<<"%"<<endl;
+    if(per >=50){
+        cout<<"Status: PASS"<<endl;
+
+    }
+    else{
+        cout<<"Status: FAIL"<<endl;
+
+    }
+
+}
+int main(){
+    char press, select;
+    do{
+        system("CLS");
+        cout<<"***********"<<endl;
+        cout<<"Quiz System"<<endl;
+        cout<<"***********"<<endl;
+        cout<<"\n\nEnter your name"<<endl;
+        fgets(st.name, sizeof(st.name), stdin);
+        
+        cout<<"\n\nEnter your roll no."<<endl;
+        fgets(st.roll_no, sizeof(st.roll_no), stdin);
+        system("CLS");
+        cout<<"Marks less than 50% will be considered Fail"<<endl;
+        cout<<"\t\tSelect which subject quiz you want to perform"<<endl;
+        cout<<"1. C++"<<endl;
+        cout<<"2. Python"<<endl;
+        cout<<"3. C#"<<endl;
+        select =getch();
+        system("CLS");
+
+
+        switch(select){
+            case '1':
+            Cpp();
+            system("CLS");
+            cout<<"C++ Quiz Result"<<endl;
+            result();
+        
+            break;
+            case '2':
+            
+            Python();
+            system("CLS");
+            cout<<"Python Quiz Result"<<endl;
+            result();
+            break;
+            case '3':
+            Csharp();
+            cout<<"C# Quiz Result"<<endl;
+            result();
+            break;
+
+        }
+        cout<<"Press y to continue to any key to exit"<<endl;
+        press =getch();
+
+    }   
+    while(press =='y' || press =='Y');
+
+
+
+    return 0;
+
+}
